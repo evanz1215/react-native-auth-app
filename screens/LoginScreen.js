6e628fc3751e4a95f86d41a902559dd3aa17ba32
Evan <evanz1215@gmail.com>
2024-05-12 17:00:56 +0800

    error handler

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -3,14 +3,23 @@ import { useState } from "react";
 import AuthContent from "../components/Auth/AuthContent";
 import LoadingOverlay from "../components/ui/LoadingOverlay";
 import { login } from "../util/auth";
+import { Alert } from "react-native";
 
 function LoginScreen() {
   const [isAuthenticating, setIsAuthenticating] = useState(false);
 
   async function loginHandler({ email, password }) {
-    setIsAuthenticating(true);
-    await login(email, password);
-    setIsAuthenticating(false);
+    try {
+      setIsAuthenticating(true);
+      await login(email, password);
+    } catch (error) {
+      Alert.alert(
+        "Authenticating failed!",
+        "Could not log you in. Please try again."
+      );
+    } finally {
+      setIsAuthenticating(false);
+    }
   }
 
   if (isAuthenticating) {
